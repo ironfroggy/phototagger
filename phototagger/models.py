1ff98a48449ffb14eaa3bb2ad667df72685746d1
Calvin Spealman <ironfroggy@gmail.com>
2010-05-10 08:22:17 -0400

    Adjust the crop ratios for use with the original photos, providing a higher quality result.

--- phototagger/models.py
+++ phototagger/models.py
@@ -29,15 +29,29 @@ class PhotoBox(models.Model):
                 'height': height,
                 'width': width,
             }
-        real_w, real_h = self.photo.get_display_size()
+        real_w = self.width
+        real_h = self.height
+
+        # The crop selection was made against the display image, so that on very large images it is still usable
+        # The crop selection needs to be scaled to the real image size
+        x_scale = real_w / self.photo.image.width
+        y_scale = real_h / self.photo.image.height
+
+        right = (self.width + self.x) * x_scale
+        bottom = (self.width + self.y) * y_scale
+        top = self.y * y_scale
+        left = self.x * x_scale
+        pos_top = -top
+        pos_left = -left
+
         return mark_safe('''<img src="%(src)s" style="position: absolute; %(hw)s clip:rect(%(top)dpx %(right)dpx %(bottom)dpx %(left)dpx);" class="clipexpand" data-real-width="%(real_w)d" data-real-height="%(real_h)d" %(extra)s />''' % {
-            'src': self.photo.get_display_url(),
-            'top': self.y,
-            'right': self.width + self.x,
-            'bottom': self.height + self.y,
-            'left': self.x,
-            'pos_top': -self.y,
-            'pos_left': -self.x,
+            'src': self.photo.image.url,
+            'top': top,
+            'right': right,
+            'bottom': bottom,
+            'left': left,
+            'pos_top': -pos_top,
+            'pos_left': -pos_left,
             'hw': hw,
             'real_w': real_w,
             'real_h': real_h,
